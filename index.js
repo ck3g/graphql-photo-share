7252c8ac5bb8d01ef85293e1806066aac460ff4c
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-15 18:00:14 +0100

    Configure MongoDB

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
 
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
 const { readFileSync } = require('fs')
 
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 const resolvers = require('./resolvers')
 
-var app = express()
 
-const server = new ApolloServer({ typeDefs, resolvers })
-server.applyMiddleware({ app })
+async function start() {
+  const app = express()
+  const MONGO_DB = process.env.DB_HOST
 
-app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+  const client = await MongoClient.connect(
+    MONGO_DB,
+    { useNewUrlParser: true }
+  )
+  const db = client.db()
 
-app.listen({ port: 4000 }, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`))
+  const context = { db }
+
+  const server = new ApolloServer({ typeDefs, resolvers, context })
+  server.applyMiddleware({ app })
+
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+
+  app.listen({ port: 4000 }, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`))
+}
+
+start()
